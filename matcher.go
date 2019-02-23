66dc539f34edf5f53b83e26d9685b31d24e4adcc
Marin <marin.basic02@gmail.com>
2019-02-23 15:07:48 +0100

    Added matcher helper function

---
+++ matcher.go
@@ -0,0 +1,10 @@
+package btcvanity
+
+import "strings"
+
+// isMatch checks if the btc wallet matches the prefix of pattern
+func isMatch(pattern string, addr string) bool {
+	addr = strings.ToLower(addr[1 : len(addr)-1])
+	pattern = strings.ToLower(pattern)
+	return strings.HasPrefix(addr, pattern)
+}
