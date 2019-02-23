fd587cae4e5a5e43c7c575005e2453243fa7c865
Marin <marin.basic02@gmail.com>
2019-02-23 15:07:26 +0100

    Added bitcoin generator function

---
+++ generator.go
@@ -0,0 +1,37 @@
+package btcvanity
+
+import (
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil"
+)
+
+// Generator hold the logic for generating bitcoin address
+// Depending on chain configuration, it can generate main/test net versions of wallet
+type Generator struct {
+	params *chaincfg.Params
+}
+
+// Generate generates bitcoin wallet interface
+func (g *Generator) Generate() (IWallet, error) {
+	wallet := &Wallet{}
+	var err error
+
+	privKey, err := btcec.NewPrivateKey(btcec.S256())
+	if err != nil {
+		return nil, err
+	}
+
+	wallet.privKey, err = btcutil.NewWIF(privKey, g.params, false)
+	if err != nil {
+		return nil, err
+	}
+
+	wallet.pubKey, err = btcutil.NewAddressPubKey(
+		wallet.privKey.PrivKey.PubKey().SerializeUncompressed(), g.params)
+	if err != nil {
+		return nil, err
+	}
+
+	return wallet, nil
+}
