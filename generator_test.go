9adec26e76d715e6170e8f79f0aef45c8f36dd8c
Marin <marin.basic02@gmail.com>
2019-02-24 17:51:30 +0100

    Updated generator test

--- generator_test.go
+++ generator_test.go
@@ -24,7 +24,14 @@ func TestGenerator(t *testing.T) {
 	}
 
 	pubKye := w.PublicKey()
+	privKey := w.PrivateKey()
 	t.Logf("%v\n", pubKye)
+	t.Logf("%v\n", privKey)
+
+	if len(privKey) != 51 {
+		t.Errorf("Wrong size of private key, expected 51 got %v\n", len(privKey))
+		return
+	}
 
 	// regex to see if this is bitcoin adddress
 	matched, err := regexp.MatchString("^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$", pubKye)
@@ -36,4 +43,5 @@ func TestGenerator(t *testing.T) {
 		t.Errorf("This is not generated bitcoin address %v\n", pubKye)
 		return
 	}
+
 }
