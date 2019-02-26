50fda2b57d6deaad3b178cbc68a6c320ed61caf6
Marin <marin.basic02@gmail.com>
2019-02-26 12:45:09 +0100

    Added test

---
+++ btcvanity_test.go
@@ -0,0 +1,59 @@
+package btcvanity
+
+import (
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestBTCVanity(t *testing.T) {
+
+	b := New(&Config{
+		Threads: 5,
+	})
+
+	wallet, err := b.Find("ab")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	pubKey := strings.ToLower(wallet.PublicKey())
+	expected := "1ab"
+
+	hasPrefix := strings.HasPrefix(pubKey, expected)
+	if !hasPrefix {
+		t.Errorf("Wrong generated wallet, got %v expected %v\n", pubKey, expected)
+	}
+
+	t.Logf("Generated address %v\n", pubKey)
+
+	done := make(chan bool)
+	// this is going to take billion of years
+	go func() {
+		wallet, err = b.Find("helloworld")
+		if err != nil {
+			t.Error(err)
+			t.Fail()
+		}
+		if wallet != nil {
+			t.Error("We got generated wallet! Should not happen")
+			t.Fail()
+		}
+		done <- true
+	}()
+
+	// wait and stop the process
+loop:
+	for {
+		select {
+		case <-done:
+			t.Log("Process stopped")
+			break loop
+		case <-time.After(2 * time.Second):
+			b.Stop()
+			break
+		}
+	}
+
+}
